88cb418cd459c1d6059baf116218ebc6d252a41a
myeongsu-ko <rhaudtn1234@naver.com>
2020-12-17 08:59:00 +0900

    aaa

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import EventPractice from './EventPractice';
+import Say from './Say';
+import ValidationSample from './ValidationSample';
+
+function App() {
+  return (
+    <div>
+      <Say/>
+      <EventPractice/>
+      <ValidationSample/>
+    </div>
+  );
+}
+
+export default App;
